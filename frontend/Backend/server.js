9bf9aa9527b408bd80e2497bc8cc1714f98a71ea
chamuditha01 <heshanchamuditha05@gmail.com>
2024-05-21 12:38:42 +0530

    port change

--- frontend/Backend/server.js
+++ frontend/Backend/server.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Use environment variable for port
+const PORT =  5000; // Use environment variable for port
 
 // Middleware for handling CORS
 app.use(cors());
