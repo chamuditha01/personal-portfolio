6994ee493cea8acd155aa5d36e4285f2939ecc8a
chamuditha01 <heshanchamuditha05@gmail.com>
2024-06-21 14:47:19 +0530

    ok

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -14,7 +14,7 @@ export const Profile = () => {
 
   const fetchSkills = async () => {
     try {
-      const response = await axios.get('https://portfolio-backend-phi-three.vercel.app/getSkills');
+      const response = await axios.get('https://portfolio-backend-chi-liart.vercel.app/getskills');
       setGetskill(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
